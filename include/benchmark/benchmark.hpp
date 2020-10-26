0146b082ac94e0f32bb73c29d8a0fb7e7a46e71c
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-10-25 22:22:00 -0500

    Added benchmark class and block progress bar

--- include/benchmark/benchmark.hpp
+++ include/benchmark/benchmark.hpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <chrono>
 #include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <numeric>
 #include <optional>
@@ -46,12 +47,12 @@ class benchmark {
     if (early_estimate_execution_time < 100) {
       // tens of nanoseconds
       num_iterations_ = 128000;
-      max_num_runs_ = 100000;
+      max_num_runs_ = 10000;
     }
     else if (early_estimate_execution_time < 1000) {
       // hundreds of nanoseconds
       num_iterations_ = 64000;
-      max_num_runs_ = 10000;
+      max_num_runs_ = 5000;
     }
     else if (early_estimate_execution_time < 1000000) {
       // microseconds
@@ -73,7 +74,7 @@ class benchmark {
   std::string duration_to_string(const long double& ns) {
     std::stringstream os;
     if (ns < 1E3) {
-      os << ns << "ns";
+      os << std::setprecision(3) << ns << "ns";
     }
     else if (ns < 1E6) {
       os << std::setprecision(3) << (ns / 1E3) << "us";
@@ -89,53 +90,47 @@ class benchmark {
 
 public:
 
-  benchmark(Fn fn) {
+  benchmark(const std::string& name, Fn fn) {
     using namespace std::chrono;
-    using namespace indicators;
 
     update_iterations(fn);
-
-    // Hide cursor
-    indicators::show_console_cursor(false);
-
-    BlockProgressBar bar{
-      option::BarWidth{20},
-      option::ForegroundColor{Color::white},
-      option::FontStyles{
-            std::vector<FontStyle>{FontStyle::bold}},
-      option::MaxProgress{max_num_runs_}
-    };
 
     long double lowest_rsd = 100;
     std::size_t num_iterations_lowest_rsd = 0;
     long double mean_lowest_rsd = 0;
 
-    std::size_t num_runs = 1;
+    std::size_t num_runs = 0;
 
     std::vector<long double> durations;
     durations.reserve(num_iterations_);
 
+    // Hide cursor
+    indicators::show_console_cursor(false);
+
+    using namespace indicators;
+    BlockProgressBar bar{
+      option::BarWidth{20},
+      option::PrefixText{name + " "},
+      option::ForegroundColor{Color::white},
+      option::FontStyles{std::vector<FontStyle>{FontStyle::bold}},
+      option::MaxProgress{max_num_runs_}
+    };
+
     while(true) {
 
       update_iterations(fn);
+
       bar.set_option(option::MaxProgress{max_num_runs_});
 
-      const auto warmup = num_iterations_ * 0.2; // 20%
-
-      // Warm-up runs
-      for (std::size_t i = 0; i < warmup; i++) {
-        fn();
-      }
-
       // Benchmark runs
-      for (std::size_t i = warmup; i < num_iterations_; i++) {
+      for (std::size_t i = 0; i < num_iterations_; i++) {
         const auto start = high_resolution_clock::now();
         fn();
         const auto end = high_resolution_clock::now();
         const auto execution_time = duration_cast<std::chrono::nanoseconds>(end - start).count();
         durations.push_back(execution_time);
       }
-      auto size = num_iterations_ - warmup;
+      auto size = num_iterations_;
       const long double mean = std::accumulate(durations.begin(), durations.end(), 0.0) / size;
 
       long double E = 0;
@@ -149,61 +144,33 @@ public:
       // Save record of lowest RSD
       const auto current_lowest_rsd = lowest_rsd;
       lowest_rsd = std::min(relative_standard_deviation, lowest_rsd);
-      if (lowest_rsd < current_lowest_rsd && num_iterations_ >= num_iterations_lowest_rsd) {
+      if (lowest_rsd < current_lowest_rsd) {
         num_iterations_lowest_rsd = num_iterations_;
         mean_lowest_rsd = mean;
       }
 
-      durations.clear();
-      num_runs += 1;
-
-      std::stringstream os;
-      os << num_runs << "/" << max_num_runs_
-         << std::setprecision(3)
-         << " | Current Estimate: μ = " 
-         << duration_to_string(mean_lowest_rsd) << " ± " << lowest_rsd
-         << "%, N = " << num_iterations_lowest_rsd;
-
-      // Show iteration as postfix text
-      bar.set_option(option::PostfixText{
-        os.str()
-      });
-
-      if (num_runs + 1 == max_num_runs_) {
-        // next is last
-        if (lowest_rsd > 10) { // too high, not reliable
-          num_runs = 0;
-          lowest_rsd = 100;
-          num_iterations_lowest_rsd = 0;
-          mean_lowest_rsd = 0;
-        }
-      }
-
-      // update progress bar
       bar.set_progress(num_runs);
 
-      if (num_runs == max_num_runs_) {
+      // Show iteration as postfix text
+      std::stringstream os;
+      os
+        << num_runs << "/" << max_num_runs_ << " "
+        << std::setprecision(3)
+        << "μ = "
+        << duration_to_string(mean_lowest_rsd) << " ± " << lowest_rsd
+        << "%, N = " << num_iterations_lowest_rsd;
+      bar.set_option(option::PostfixText{os.str()});
+
+      if (num_runs >= max_num_runs_) {
+        bar.mark_as_completed();
         break;
       }
+
+      durations.clear();
+      num_runs += 1;
     }
 
-    std::stringstream os;
-    os << num_runs << "/" << max_num_runs_
-        << std::setprecision(3)
-        << " | Best Estimate: μ = " 
-        << mean_lowest_rsd << "ns ± " << lowest_rsd
-        << "%, N = " << num_iterations_lowest_rsd;
-
-    // Show iteration as postfix text
-    bar.set_option(option::PostfixText{
-      os.str()
-    });
-
     // Show cursor
-    show_console_cursor(true);
-  }
-
-  ~benchmark() {
     indicators::show_console_cursor(true);
   }
 };
--- include/benchmark/main.cpp
+++ include/benchmark/main.cpp
@@ -3,42 +3,54 @@
 #include <vector>
 #include <unordered_map>
 
-// 10
-// Num iterations  : 128000
-// Mean            : 39.6844
-// Relative SD.    : 1.52863
-
-std::vector<size_t> v(1000, 0);
-
-auto sort_vector() {
-	std::fill(v.begin(), v.end(), 0);	
-	std::sort(v.begin(), v.end());
-}
-
 int main() {
-	// auto b1 = benchmark(sort_vector);
 
-	// [████████████████████] 100% 10001/10000 | Best Estimate: μ = 526ns ± 1.88%, N = 64000
-	// auto b2 = benchmark([]() {
-	// 	std::unordered_map<std::string, int> map;
-	// 	map["a"] = 1;
-	// 	map["b"] = 2;
-	// 	map["c"] = 3;
-	// 	map["a"] = 0;
-	// });
+	// {
+	//   std::vector<size_t> v(10000, 0);
+	//   auto b = benchmark([&v] {
+	//     std::fill(v.begin(), v.end(), 0);	
+	//     std::sort(v.begin(), v.end());
+	//   });
+	// }
 
-	// auto b3 = benchmark([] {
-	// 	return std::to_string(3.14);
-	// });
+	{
+		// [████████████████████] 100% 10001/10000 | Best Estimate: μ = 526ns ± 1.88%, N = 64000
+		benchmark("Benchmark #1", []() {
+			std::unordered_map<std::string, int> map;
+			map["a"] = 1;
+			map["b"] = 2;
+			map["c"] = 3;
+			map["a"] = 0;
+		});
+	}
 
-	auto b4 = benchmark([] {
-		volatile int i;
-		std::vector<std::string> result;
-		std::string foo;
-		for (i = 0; i < 100; i++) {
-			foo = std::string{"Hello World "} + std::to_string(i);
-			result.push_back(foo);
-		}
-		return result;
-	});
+	// {
+	// 	// 5000/5000 μ = 168ns ± 2.79%, N = 64000
+	// 	auto b = benchmark([] {
+	// 		return std::to_string(3.14);
+	// 	});
+	// }
+
+	// {
+	// 	// 1000/1000 μ = 3.23us ± 15.1%, N = 32000
+	// 	auto b = benchmark([] {
+	// 		volatile int i;
+	// 		std::vector<std::string> result;
+	// 		std::string foo;
+	// 		for (i = 0; i < 100; i++) {
+	// 			foo = std::string{"Hello World "} + std::to_string(i);
+	// 			result.push_back(foo);
+	// 		}
+	// 		return result;
+	// 	});
+	// }
+
+	// {
+	// 	// 1038/1000 μ = 1.04us ± 1.21%, N = 32000
+	// 	auto b = benchmark([] {
+	// 		std::vector<int> numbers(1000, 2);
+	// 		std::sort(numbers.begin(), numbers.end());
+	// 		return std::accumulate(numbers.begin(), numbers.end(), 0);
+	// 	});
+	// }
 }
