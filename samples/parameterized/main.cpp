a0138ea0fbe8963010491ccc792dc564ba50b121
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-10-27 19:42:34 -0500

    Minor update for readability

--- samples/parameterized/main.cpp
+++ samples/parameterized/main.cpp
@@ -5,15 +5,14 @@
 int main() {
   std::vector<size_t> sizes = {10, 20, 30, 40, 50};
 
-  std::for_each(sizes.begin(), sizes.end(), 
-    [](size_t &n) { 
-      const auto name = std::string{"vector_construct_sort/"} + std::to_string(n);
-      benchmark(name, 
-      [&n] {
-        auto vec = std::vector<int>(n, 0);
-        std::sort(vec.begin(), vec.end());
-      });
-    }
-  );
+  auto run_benchmark = [](size_t &n) { 
+    const auto name = std::string{"vector_construct_sort/"} + std::to_string(n);
+    benchmark(name, 
+    [&n] {
+      auto vec = std::vector<int>(n, 0);
+      std::sort(vec.begin(), vec.end());
+    });
+  };
 
+  std::for_each(sizes.begin(), sizes.end(), run_benchmark);
 }
