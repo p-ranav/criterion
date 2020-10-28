af0c958ddc2d74d0acf7f766865c0e089d8ce640
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-10-28 17:57:26 -0500

    Added container forward sample

---
+++ samples/container_forward/container_forward.cpp
@@ -0,0 +1,37 @@
+#include <benchmark/registration.hpp>
+#include <list>
+#include <vector>
+
+BENCHMARK("std::list<int>.forward")
+{
+  SETUP_BENCHMARK(
+    std::list<int> list;
+    for (int i = 0; i < 1000000; ++i)
+      list.push_back(rand());
+  )
+
+  // Code being benchmarked
+  for (auto it = list.begin(); it != list.end(); ++it)
+    ++(*it);
+
+  TEARDOWN_BENCHMARK(
+    list.clear();
+  )
+}
+
+BENCHMARK("std::vector<int>.forward")
+{
+  SETUP_BENCHMARK(
+    std::vector<int> vec;
+    for (int i = 0; i < 1000000; ++i)
+      vec.push_back(rand());
+  )
+
+  // Code being benchmarked
+  for (auto it = vec.begin(); it != vec.end(); ++it)
+    ++(*it);
+
+  TEARDOWN_BENCHMARK(
+    vec.clear();
+  )
+}
