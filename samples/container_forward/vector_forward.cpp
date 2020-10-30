fd5ccbf4310e41a023fbf70dcb6abfaa9cf584b7
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-10-30 18:40:41 -0500

    Updated formatting for vector_forward sample

--- samples/container_forward/vector_forward.cpp
+++ samples/container_forward/vector_forward.cpp
@@ -1,12 +1,19 @@
 #include <criterion/criterion.hpp>
 #include <vector>
 
-BENCHMARK(VectorIntForward) {
-  SETUP_BENCHMARK(std::vector<int> vec; for (int i = 0; i < 1000000; ++i) vec.push_back(rand());)
+BENCHMARK(VectorIntForward)
+{
+  SETUP_BENCHMARK(
+    std::vector<int> vec;
+    for (int i = 0; i < 1000000; ++i)
+      vec.push_back(rand());
+  )
 
   // Code being benchmarked
   for (auto it = vec.begin(); it != vec.end(); ++it)
     ++(*it);
 
-  TEARDOWN_BENCHMARK(vec.clear();)
+  TEARDOWN_BENCHMARK(
+    vec.clear();
+  )
 }
