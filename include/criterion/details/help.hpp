20909d55ce0e2b6bbc1fd5c70c57f59d6fd672cb
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-11-01 14:40:41 -0600

    Added pretty help printer function for criterion

---
+++ include/criterion/details/help.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include <sstream>
+#include <criterion/details/indicators.hpp>
+
+static inline void print_criterion_help() {
+  std::cout << "\n";
+  std::cout << termcolor::bold << "NAME\n" << termcolor::reset;
+  std::cout << "     " << termcolor::bold << "criterion" << termcolor::reset << " -- Run Criterion benchmarks\n";
+  std::cout << "\n";
+  std::cout << termcolor::bold << "SYNOPSIS\n" << termcolor::reset;
+  std::cout << termcolor::bold << "     criterion " << termcolor::reset 
+            << "[" << termcolor::bold << "--export_results" << termcolor::reset 
+            << " {csv,json} <filename>]\n";
+  std::cout << "\n";
+  std::cout << termcolor::bold << "DESCRIPTION\n" << termcolor::reset;
+  std::cout << "     The " << termcolor::bold << "criterion" << termcolor::reset 
+            << " microbenchmarking utility repeatedly executes a list of registered functions,\n     statistically analyzing the temporal behavior of code\n";
+  std::cout << "\n";
+  std::cout << "     The options are as follows:\n";
+  std::cout << "\n";
+  std::cout << termcolor::bold << "     --export_results " << termcolor::reset 
+            << termcolor::underline << "format" << termcolor::reset
+            << " "
+            << termcolor::underline << "filename" << termcolor::reset << "\n";
+  std::cout << "          Export benchmark results to file. The following are the supported formats.\n";
+  std::cout << "\n";
+  std::cout << "          " << termcolor::bold << "csv" << termcolor::reset << "       Comma separated values (CSV) delimited text file\n";
+  std::cout << "          " << termcolor::bold << "json" << termcolor::reset << "      JavaScript Object Notation (JSON) text file\n";
+  std::cout << "\n";
+  std::cout << termcolor::bold << "     --help " << termcolor::reset << "\n";
+  std::cout << "          Print this help message\n";
+} 
