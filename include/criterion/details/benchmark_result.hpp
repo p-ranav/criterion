0f24ec20a73d506a74213428fa96f09d0162fd7d
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-11-01 14:42:12 -0600

    Added helper functions in benchmark_result to export to CSV/JSON

--- include/criterion/details/benchmark_result.hpp
+++ include/criterion/details/benchmark_result.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <sstream>
 #include <string>
 
 namespace criterion {
@@ -12,6 +13,50 @@ struct benchmark_result {
   long double best_estimate_rsd;
   long double overall_best_execution_time;
   long double overall_worst_execution_time;
+
+  static std::string csv_header() {
+    return "name,warmup_runs,benchmark_runs,iterations_per_run,best_estimate_mean,best_estimate_rsd,overall_best_execution_time,overall_worst_execution_time";
+  }
+
+  std::string to_csv() const {
+    std::stringstream os;
+
+    os << '"' << name 
+       << "\",";
+    os << std::setprecision(3)
+       << num_warmup_runs 
+       << ','
+       << num_runs
+       << ','
+       << num_iterations
+       << ','
+       << best_estimate_mean
+       << ','
+       << best_estimate_rsd
+       << ','
+       << overall_best_execution_time
+       << ','
+       << overall_worst_execution_time;
+
+    return os.str();
+  }
+
+  std::string to_json() const {
+    std::stringstream os;
+
+    os << "    {\n";
+    os << "      \"name\": \"" << name << "\",\n";
+    os << "      \"warmup_runs\": " << num_warmup_runs << ",\n";
+    os << "      \"benchmark_runs\": " << num_runs << ",\n";
+    os << "      \"iterations_per_run\": " << num_iterations << ",\n";
+    os << "      \"best_estimate_mean\": " << best_estimate_mean << ",\n";
+    os << "      \"best_estimate_rsd\": " << best_estimate_rsd << ",\n";
+    os << "      \"overall_best_execution_time\": " << overall_best_execution_time << ",\n";
+    os << "      \"overall_worst_execution_time\": " << overall_worst_execution_time << "\n";
+    os << "    }";
+
+    return os.str();
+  }
 };
 
 } // namespace criterion
